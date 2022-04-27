ccff286f6a7fc4254361e218fefc30d10975101e
Revika Mukola <revikanikolaj@gmail.com>
2022-04-27 10:16:45 +0200

    Styles Contacts page

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,18 +1,26 @@
+import styles from './Contacts.module.scss'
+
 const Contacts = () => (
-  <main>
-    <h2>Contacts</h2>
-    <p>
-      Student{' '}
-      <a href='https://github.com/Mykola2012' target='_blank' rel='noreferrer'>
-        Revika Mykola
-      </a>
-    </p>
-    <p>
-      Training Center by{' '}
-      <a href='https://freshcode.training/' target='_blank' rel='noreferrer'>
-        FRESHCODE
-      </a>
-    </p>
+  <main className={styles.container}>
+    <h2 className={styles.subTitle}>Contacts</h2>
+    <div className={styles.contactsWrapper}>
+      <p>
+        Student{' '}
+        <a
+          href='https://github.com/Mykola2012'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Revika Mykola
+        </a>
+      </p>
+      <p>
+        Training Center by{' '}
+        <a href='https://freshcode.training/' target='_blank' rel='noreferrer'>
+          FRESHCODE
+        </a>
+      </p>
+    </div>
   </main>
 )
 
