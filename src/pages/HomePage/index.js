c36032d6c3670dfb5806a0f623b4d87609a0f2e2
Revika Mukola <revikanikolaj@gmail.com>
2022-04-27 09:01:31 +0200

    Styles HomePage

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,6 +1,10 @@
+import styles from './HomePage.module.scss'
+import hello from '../../img/hello.png'
+
 const HomePage = () => (
-  <main>
-    <h2>HomePage</h2>
+  <main className={styles.container}>
+    <h2 className={styles.subTitle}>Welcome</h2>
+    <img className={styles.imgHello} src={hello} alt='hello' />
   </main>
 )
 
